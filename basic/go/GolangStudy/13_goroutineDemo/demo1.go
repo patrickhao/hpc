6d5bea7268199f274b13e2b281f9edf5ef40f02d
patrickhao <patrickhao@qq.com>
2023-03-30 17:05:06 +0800

    init

--- basic/go/GolangStudy/13_goroutineDemo/demo1.go
+++ basic/go/GolangStudy/13_goroutineDemo/demo1.go
@@ -21,13 +21,17 @@ func main() {
 	// 创建一个go程，去执行newTask()流程
 	go newTask()
 
+	// 主进程继续
 	fmt.Println("main goroutine exit")
 
+	// 和协程各跑各的
 	// i := 0
 	// for {
 	// 	i++
 	// 	fmt.Println("main Gorotine : i = ", i)
 	// 	time.Sleep(1 * time.Second)
 	// }
+
+	// 主进程退出，协程也会退出
 
 }
